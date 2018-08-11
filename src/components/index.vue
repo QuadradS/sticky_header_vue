733e3427cf32f376f4db1e1cc4e732c59aa9b180
Quadrad <quadrad4@gmail.com>
2018-08-12 05:27:59 +0600

    default template

---
+++ src/components/index.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        {{ title }}
+    </div>
+</template>
+
+<script>
+    export default {
+        name:'Index',
+        data:function(){
+            return({
+                title:'index'
+            })
+        }
+    }
+</script>
